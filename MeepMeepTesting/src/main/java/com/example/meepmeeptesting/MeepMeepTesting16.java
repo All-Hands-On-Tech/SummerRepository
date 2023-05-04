0311298b064162fe9dc2e372704826b231a76d7d
BHill256 <bricelikesrobots@gmail.com>
2023-05-03 19:41:20 -0700

    did somethings

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting16.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting16.java
@@ -14,27 +14,15 @@ public class MeepMeepTesting16 {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setConstraints(80, 1000, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-50, 40, 0))
-                                .splineTo(new Vector2d(-50,50),Math.toRadians(180))
-                                .splineTo(new Vector2d(-50,50.1),Math.toRadians(0))
-                                .splineTo(new Vector2d(-50,60),Math.toRadians(180))
-                                .lineToLinearHeading(new Pose2d(-55, 60, Math.toRadians(0)))
-                                .lineTo(new Vector2d(-55,40))
-                                .lineTo(new Vector2d(-42,40))
-                                .lineTo(new Vector2d(-42,52))
-                                .lineToLinearHeading(new Pose2d(-42, 46, Math.toRadians(90)))
-                                .splineTo(new Vector2d(-36,47),Math.toRadians(-90))
-                                .lineToLinearHeading(new Pose2d(-36, 47.1, Math.toRadians(90)))
-                                .splineTo(new Vector2d(-42,48),Math.toRadians(90))
-                                .lineToLinearHeading(new Pose2d(-42, 40, Math.toRadians(0)))
-                                .lineTo(new Vector2d(-31,40))
-                                .lineTo(new Vector2d(-31,50))
-                                .splineTo(new Vector2d(-31,53),Math.toRadians(180))
-                                .splineTo(new Vector2d(-31,50),Math.toRadians(0))
-                                .lineTo(new Vector2d(-31,40))
-
+                        drive.trajectorySequenceBuilder(new Pose2d(-33, -62, Math.toRadians(90)))
+                                .addDisplacementMarker(() -> {
+                                    // check signal sleeve
+                                })
+                                .forward(24)
+                                .waitSeconds(1)
                                 .build()
                 );
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
