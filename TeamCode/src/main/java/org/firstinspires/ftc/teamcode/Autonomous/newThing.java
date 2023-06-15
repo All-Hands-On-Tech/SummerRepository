c26817cb780243d36599d01d0112fbdd1b6cde48
BHill256 <bricelikesrobots@gmail.com>
2023-06-14 17:50:36 -0700

    Basic Finite State Machine Road Runner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/newThing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/newThing.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,30 +10,115 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.RoboMom;
 
+
 public class newThing extends RoboMom {
 
+    // This enum defines our "state"
+    // This is essentially just defines the possible steps our program will take
+    enum State {
+        TRAJECTORY_1,   // First, follow a splineTo() trajectory
+        TRAJECTORY_2,   // Then, follow a lineTo() trajectory
+        IDLE            // Our bot will enter the IDLE state when done
+    }
 
+    // We define the current state we're on
+    // Default to IDLE
+    State currentState = State.IDLE;
+    Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
 
     @Override
     public void runOpMode() {
-
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(startPose);
 
-        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(24)
-                .forward(24)
+        // Let's define our trajectories
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(45, -20), Math.toRadians(90))
                 .build();
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .splineToSplineHeading(new Pose2d(-24,-24,Math.toRadians(180)),Math.toRadians(90))
+        // Second trajectory
+        // Ensure that we call trajectory1.end() as the start for this one
+        Trajectory trajectory2 = drive.trajectoryBuilder(traj1.end())
+                .lineTo(new Vector2d(45, 0))
                 .build();
 
+        // Define the angle to turn at
+        double turnAngle1 = Math.toRadians(-270);
 
-        super.runOpMode();
+        // Third trajectory
+        // We have to define a new end pose because we can't just call trajectory2.end()
+        // Since there was a point turn before that
+        // So we just take the pose from trajectory2.end(), add the previous turn angle to it
+        Pose2d newLastPose = trajectory2.end().plus(new Pose2d(0, 0, turnAngle1));
+        Trajectory trajectory3 = drive.trajectoryBuilder(newLastPose)
+                .lineToConstantHeading(new Vector2d(-15, 0))
+                .build();
+
+        // Define a 1.5 second wait time
+        double waitTime1 = 1.5;
+        ElapsedTime waitTimer1 = new ElapsedTime();
+
+        // Define the angle for turn 2
+        double turnAngle2 = Math.toRadians(720);
+
         waitForStart();
 
-        drive.followTrajectory(traj1);
-        drive.followTrajectory(traj1);
-        drive.followTrajectory(traj2);
+        if (isStopRequested()) return;
+
+        // Set the current state to TRAJECTORY_1, our first step
+        // Then have it follow that trajectory
+        // Make sure you use the async version of the commands
+        // Otherwise it will be blocking and pause the program here until the trajectory finishes
+        currentState = State.TRAJECTORY_1;
+        drive.followTrajectoryAsync(traj1);
+
+        while (opModeIsActive() && !isStopRequested()) {
+            // Our state machine logic
+            // You can have multiple switch statements running together for multiple state machines
+            // in parallel. This is the basic idea for subsystems and commands.
+
+            // We essentially define the flow of the state machine through this switch statement
+            switch (currentState) {
+                case TRAJECTORY_1:
+                    // Check if the drive class isn't busy
+                    // `isBusy() == true` while it's following the trajectory
+                    // Once `isBusy() == false`, the trajectory follower signals that it is finished
+                    // We move on to the next state
+                    // Make sure we use the async follow function
+                    if (!drive.isBusy()) {
+                        currentState = State.TRAJECTORY_2;
+                        drive.followTrajectoryAsync(trajectory2);
+                    }
+                    break;
+                case TRAJECTORY_2:
+                    // Check if the drive class is busy following the trajectory
+                    // Move on to the next state, TURN_1, once finished
+                    if (!drive.isBusy()) {
+                        currentState = State.IDLE;
+                        drive.turnAsync(turnAngle1);
+                    }
+                    break;
+                case IDLE:
+                    // Do nothing in IDLE
+                    // currentState does not change once in IDLE
+                    // This concludes the autonomous program
+                    break;
+            }
+
+            // Anything outside of the switch statement will run independent of the currentState
+
+            // We update drive continuously in the background, regardless of state
+            drive.update();
+            // We update our lift PID continuously in the background, regardless of state
+
+            // Read pose
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            // Print pose to telemetry
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
+        }
     }
 }
