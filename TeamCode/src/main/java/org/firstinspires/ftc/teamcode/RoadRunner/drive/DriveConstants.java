cd1daf75967d975e40fc808069a11401322880f6
Karen <51937640+KarenZhang0@users.noreply.github.com>
2023-08-23 19:47:17 -0700

    Calibration for roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
@@ -56,9 +56,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = .017;
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.0175;
+    public static double kA = 0.0025;
+    public static double kStatic = -0.015;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
