8e44b0b4c0bff40deceafc18fdbc6c3dadd43587
HuyStenzel <Stenz00697@loswego.k12.or.us>
2023-11-04 20:09:16 -0700

    created linear slide testing
    use to get ticks for certain positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LinearSlideTesting.java
@@ -0,0 +1,74 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.DeliveryFunctions;
+import org.firstinspires.ftc.teamcode.RoboMom;
+import org.firstinspires.ftc.teamcode.Vision.AprilTagsFunctions;
+
+
+@TeleOp(name="Linear Slide Testing", group="A")
+
+
+public class LinearSlideTesting extends RoboMom {
+
+    private DeliveryFunctions deliveryFunctions;
+
+    private int leftMotorPosition;
+    private int rightMotorPosition;
+
+    @Override
+    public void runOpMode() {
+        super.runOpMode();
+        deliveryFunctions = new DeliveryFunctions(this);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            leftMotorPosition = deliveryFunctions.getMotorPositionByIndex(0);
+            rightMotorPosition = deliveryFunctions.getMotorPositionByIndex(1);
+
+            telemetry.addData("Left Ticks:", leftMotorPosition);
+            telemetry.addData("Right Ticks:", rightMotorPosition);
+        }
+
+
+    }
+
+
+}
