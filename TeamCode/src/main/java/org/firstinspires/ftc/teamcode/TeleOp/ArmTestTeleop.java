a383cf8027d3ba6b490a4246befc4ab9e79526a3
HuyStenzel <Stenz00697@loswego.k12.or.us>
2023-11-16 21:13:15 -0800

    Added basic arm test telop
    Just so we can test the hanging

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArmTestTeleop.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.DeliveryFunctions;
+import org.firstinspires.ftc.teamcode.IntakeFunctions;
+
+@TeleOp(name="Arm Test Teleop", group="A")
+public class ArmTestTeleop extends LinearOpMode {
+    private DeliveryFunctions deliveryFunctions;
+    private IntakeFunctions intakeFunctions;
+    private int leftMotorPosition;
+    private int rightMotorPosition;
+    private final double DEADZONE = 0.05;
+
+
+    public void runOpMode() {
+        deliveryFunctions = new DeliveryFunctions(this, false);
+        intakeFunctions = new IntakeFunctions(this);
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(Math.abs(gamepad2.left_stick_y) > DEADZONE){
+
+                deliveryFunctions.setSlidesPower(gamepad2.left_stick_y);
+
+            }
+
+            if(Math.abs(gamepad2.right_stick_y) > DEADZONE){
+
+                deliveryFunctions.setSlidesPower(gamepad2.right_stick_y / 3);
+
+            }
+
+            deliveryFunctions.WristMovementByLiftPosition();
+
+            if(gamepad2.right_trigger > DEADZONE){
+                intakeFunctions.RunIntakeMotor(gamepad2.right_trigger);
+            } else{
+                intakeFunctions.RunIntakeMotor(0);
+            }
+
+
+            telemetry.addData("Left Motor Position: ",deliveryFunctions.getMotorPositionByIndex(0));
+            telemetry.addData("Right Motor Position: ",deliveryFunctions.getMotorPositionByIndex(1));
+
+            //telemetry.addData("Target Motor Position ",deliveryFunctions.getMotorTargetPosition());
+
+            telemetry.update();
+
+        }
+    }
+}
