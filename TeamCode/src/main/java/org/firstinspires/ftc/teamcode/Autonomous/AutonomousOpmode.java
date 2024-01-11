663d86e1ba8de3bd5aba028abc57d85bb9152172
HuyStenzel <Stenz00697@loswego.k12.or.us>
2024-01-11 11:11:19 -0800

    Autonomous opMode parent class
    NEEDS TESTING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousOpmode.java
@@ -6,9 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.AprilTagsFunctions;
+import org.firstinspires.ftc.teamcode.DeliveryFunctions;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.RoadRunner.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.Vision.CircleDetectionPipeline;
+import org.firstinspires.ftc.teamcode.Vision.VisionConstants;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -17,7 +20,15 @@ import org.openftc.easyopencv.OpenCvWebcam;
 @Disabled
 public class AutonomousOpmode extends LinearOpMode {
 
-    private OpenCvWebcam webcam;
+    protected OpenCvWebcam webcam;
+
+    protected double fx = VisionConstants.fx;
+    protected double fy = VisionConstants.fy;
+    protected double cx = VisionConstants.cx;
+    protected double cy = VisionConstants.cy;
+    protected int RESWIDTH = VisionConstants.RESWIDTH;
+    protected int RESHEIGHT = VisionConstants.RESHEIGHT;
+    protected int TIMEOUT = 5;
     protected CircleDetectionPipeline circleDetection;
     protected SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     protected Pose2d startPose;
@@ -30,212 +41,87 @@ public class AutonomousOpmode extends LinearOpMode {
     protected TrajectorySequence right;
 
     protected AutonomousTrajectories trajectories;
+    protected AprilTagsFunctions aprilTagsFunctions;
+    protected CircleDetectionPipeline circleDetectionPipeline;
+    protected DeliveryFunctions deliveryFunctions;
 
 
-    private void Initialize() {
+    protected void Initialize(LinearOpMode l) {
+        circleDetectionPipeline = new CircleDetectionPipeline(l.telemetry, isRed);
+
+        aprilTagsFunctions = new AprilTagsFunctions(l);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        circleDetection = new CircleDetectionPipeline(isRed);
-        webcam.setPipeline(circleDetection);
-        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                webcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
-            }
-            @Override
-            public void onError(int errorCode) {
-            }
-        });
 
-        SetAppropriateTrajectories();
+        webcam.setPipeline(circleDetectionPipeline);
+
+        drive = new SampleMecanumDrive(hardwareMap);
+        deliveryFunctions = new DeliveryFunctions(l, true);
+        aprilTagsFunctions = new AprilTagsFunctions(l);
+
+
 
     }
 
-    @Override
-    public void runOpMode() {
-
-        Initialize();
-        waitForStart();
-
-        sleep(1000);
-
-        StartChecking();
-        MakeDetection(5);
-
-
-        switch (spikePosition) {
-            case "LEFT":
-                telemetry.addLine("left");
-                telemetry.update();
-                drive.followTrajectorySequence(left);
-                break;
-            case "MID":
-                telemetry.addLine("center");
-                telemetry.update();
-                drive.followTrajectorySequence(center);
-                break;
-            case "RIGHT":
-                telemetry.addLine("right");
-                telemetry.update();
-                drive.followTrajectorySequence(right);
-                break;
-        }
-
-    }
-
-    public String MakeDetection(int timeoutInSeconds) {
+    protected String MakeDetection(int timeoutInSeconds) {
         int tries = 0;
-        while (opModeIsActive() && !circleDetection.isDetected() && tries < timeoutInSeconds * 10) {
-            sleep(100);
+        while (opModeIsActive() && !circleDetectionPipeline.isDetected() && tries < timeoutInSeconds * 10) {
+            sleep(50);
             tries++;
             telemetry.addData("Detection tries:", tries);
         }
-        if (!circleDetection.isDetected()){
+        if (!circleDetectionPipeline.isDetected()){
+            telemetry.addLine("Defaulted");
+
             return "MID";
         } else{
-            return circleDetection.spikePosition;
+            return circleDetectionPipeline.spikePosition;
         }
     }
 
-    private void StartChecking(){
-        circleDetection.setState(CircleDetectionPipeline.DetectionState.DETECT);
+    //    if (aprilTagsFunctions.DetectAprilTag(aprilTagsFunctions.BLUE_1_TAG)) {
+//        telemetry.addData("Found", "ID %d (%s)", aprilTagsFunctions.detectedTag.id, aprilTagsFunctions.detectedTag.metadata.name);
+//        telemetry.addData("Range", "%5.1f inches", aprilTagsFunctions.detectedTag.ftcPose.range);
+//        telemetry.addData("Bearing", "%3.0f degrees", aprilTagsFunctions.detectedTag.ftcPose.bearing);
+//        telemetry.addData("Yaw", "%3.0f degrees", aprilTagsFunctions.detectedTag.ftcPose.yaw);
+//        telemetry.addData("X delta", "%3.0f inches", aprilTagsFunctions.detectedTag.ftcPose.x);
+//
+//        if (gamepad1.right_trigger > 0.025f) {
+//            rightTriggerPull = gamepad1.right_trigger;
+//
+////                    strafeGain *= rightTriggerPull;
+////                    forwardGain *= rightTriggerPull;
+////                    rotationGain *= rightTriggerPull;
+//
+//            double x = STRAFE_GAIN * aprilTagsFunctions.detectedTag.ftcPose.yaw;
+//            double y = -FORWARD_GAIN * aprilTagsFunctions.detectedTag.ftcPose.range;
+//
+////                    double x = 0.5;
+////                    double y = 0.7;
+//            double bearing = -ROTATION_GAIN * aprilTagsFunctions.detectedTag.ftcPose.bearing;
+//
+//            telemetry.addData("x: ", x);
+//            telemetry.addData("y: ", y);
+//            telemetry.addData("bearing: ", bearing);
+//
+//            drivetrainFunctions.Move((float) x, (float) y, (float) bearing, 1);
+//        } else {
+//            controlsRelinquished = false;
+//        }
+//
+//                /*
+//                if (currentGamepad1.right_trigger > 0.5) {
+//                    y      = SPEED_GAIN * (aprilTagsFunctions.detectedTag.ftcPose.range - DESIRED_DISTANCE_TO_APRIL_TAG_INCHES);
+//                    yaw    = -TURN_GAIN * aprilTagsFunctions.detectedTag.ftcPose.yaw;
+//                    x      = STRAFE_GAIN * aprilTagsFunctions.detectedTag.ftcPose.x;
+//                    isAutoDrivingToAprilTag = true;
+//                }
+//                 */
+//    } else {          //align to point (pose of aprilTag)
+//
+//    }
+    public void runOpMode() {
+        waitForStart();
     }
-
-    private void SetAppropriateTrajectories(){
-        if(isRed){
-            if(isBackstage){
-                left = RedBackstageLeftTrajectory;
-                center = RedBackstageCenterTrajectory;
-                right = RedBackstageRightTrajectory;
-            }else{
-                left = RedLandingZoneLeftTrajectory;
-                center = RedLandingZoneCenterTrajectory;
-                right = RedLandingZoneRightTrajectory;
-
-            }
-        } else{
-            if(isBackstage){
-                left = BlueBackstageLeftTrajectory;
-                center = BlueBackstageCenterTrajectory;
-                right = BlueBackstageRightTrajectory;
-            } else{
-                left = BlueLandingZoneLeftTrajectory;
-                center = BlueLandingZoneCenterTrajectory;
-                right = BlueLandingZoneRightTrajectory;
-            }
-
-        }
-    }
-
-
-    private Pose2d RED_BACKSTAGE_START_POSE = new Pose2d(60, 14, Math.toRadians(180));
-    private Pose2d RED_LANDING_ZONE_START_POSE = new Pose2d(60, -38, Math.toRadians(180));
-    private Pose2d BLUE_BACKSTAGE_START_POSE = new Pose2d(-60, 14, Math.toRadians(0));
-    private Pose2d BLUE_LANDING_ZONE_START_POSE = new Pose2d(-60, -38, Math.toRadians(0));
-
-
-    //BLUE BACKSTAGE
-    public TrajectorySequence BlueBackstageLeftTrajectory = drive.trajectorySequenceBuilder(BLUE_BACKSTAGE_START_POSE)
-            .splineTo(new Vector2d(-36, 23), Math.toRadians(0))
-            .back(10)
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(-55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-    public TrajectorySequence BlueBackstageCenterTrajectory = drive.trajectorySequenceBuilder(BLUE_BACKSTAGE_START_POSE)
-            .splineTo(new Vector2d(-30, 11), Math.toRadians(0))
-            .back(10)
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(-55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-    public TrajectorySequence BlueBackstageRightTrajectory = drive.trajectorySequenceBuilder(BLUE_BACKSTAGE_START_POSE)
-            .splineToLinearHeading(new Pose2d(-32, 10, Math.toRadians(-90)), Math.toRadians(-90))
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(-55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-
-    //BLUE LANDING ZONE
-    public TrajectorySequence BlueLandingZoneRightTrajectory = drive.trajectorySequenceBuilder(BLUE_LANDING_ZONE_START_POSE)
-            .splineToConstantHeading(new Vector2d(-38, -47), Math.toRadians(0))
-            .back(7)
-            .strafeLeft(12.5)
-            .forward(33)
-            .lineToLinearHeading(new Pose2d(-8, 50, Math.toRadians(0)))
-            .build();
-
-    public TrajectorySequence BlueLandingZoneCenterTrajectory = drive.trajectorySequenceBuilder(BLUE_LANDING_ZONE_START_POSE)
-            .splineTo(new Vector2d(-31, -35), Math.toRadians(0))
-            .back(5)
-            .strafeRight(15)
-            .forward(25)
-            .lineToLinearHeading(new Pose2d(-10, 50, Math.toRadians(0)))
-            .build();
-
-    public TrajectorySequence BlueLandingZoneLeftTrajectory = drive.trajectorySequenceBuilder(BLUE_LANDING_ZONE_START_POSE)
-            .splineToLinearHeading(new Pose2d(-30, -34, Math.toRadians(90)), Math.toRadians(90))
-            .back(5)
-            .strafeRight(18)
-            .lineToLinearHeading(new Pose2d(-12, 50, Math.toRadians(90)))
-            .build();
-
-
-
-
-    //RED BACKSTAGE
-    public TrajectorySequence RedBackstageLeftTrajectory = drive.trajectorySequenceBuilder(RED_BACKSTAGE_START_POSE)
-            .splineToLinearHeading(new Pose2d(29, 10, Math.toRadians(-90)), Math.toRadians(-90))
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-    public TrajectorySequence RedBackstageCenterTrajectory = drive.trajectorySequenceBuilder(RED_BACKSTAGE_START_POSE)
-            .splineTo(new Vector2d(30, 11), Math.toRadians(180))
-            .back(10)
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-    public TrajectorySequence RedBackstageRightTrajectory = drive.trajectorySequenceBuilder(RED_BACKSTAGE_START_POSE)
-            .splineTo(new Vector2d(36, 23), Math.toRadians(180))
-            .back(10)
-            .waitSeconds(1)
-            .setReversed(true)
-            .splineToLinearHeading(new Pose2d(55, 50, Math.toRadians(-90)), Math.toRadians(90))
-            .build();
-
-
-    //RED LANDING ZONE
-    public TrajectorySequence RedLandingZoneLeftTrajectory = drive.trajectorySequenceBuilder(RED_LANDING_ZONE_START_POSE)
-            .splineToConstantHeading(new Vector2d(38, -47), Math.toRadians(180))
-            .back(7)
-            .strafeRight(12.5)
-            .forward(33)
-            .lineToLinearHeading(new Pose2d(8, 50, Math.toRadians(180)))
-            .build();
-
-    public TrajectorySequence RedLandingZoneCenterTrajectory = drive.trajectorySequenceBuilder(RED_LANDING_ZONE_START_POSE)
-            .splineTo(new Vector2d(31, -35), Math.toRadians(180))
-            .back(5)
-            .strafeLeft(15)
-            .forward(27)
-            .turn(Math.toRadians(90))
-            .back(95)
-            .build();
-
-    public TrajectorySequence RedLandingZoneRightTrajectory = drive.trajectorySequenceBuilder(RED_LANDING_ZONE_START_POSE)
-            .splineToLinearHeading(new Pose2d(30, -34, Math.toRadians(90)), Math.toRadians(90))
-            .back(5)
-            .strafeLeft(18)
-            .lineToLinearHeading(new Pose2d(12, 50, Math.toRadians(90)))
-            .build();
-
 }
