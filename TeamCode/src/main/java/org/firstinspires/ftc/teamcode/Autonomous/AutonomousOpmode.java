80253bb70e3dbe0c9615e0cc92ebc032fb4c7098
HuyStenzel <Stenz00697@loswego.k12.or.us>
2024-01-22 10:49:10 -0800

    cleaned autonomous opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousOpmode.java
@@ -20,7 +20,7 @@ import org.openftc.easyopencv.OpenCvWebcam;
 @Disabled
 public class AutonomousOpmode extends LinearOpMode {
 
-    protected OpenCvWebcam webcam;
+//    protected OpenCvWebcam webcam;
 
     protected double fx = VisionConstants.fx;
     protected double fy = VisionConstants.fy;
@@ -29,7 +29,7 @@ public class AutonomousOpmode extends LinearOpMode {
     protected int RESWIDTH = VisionConstants.RESWIDTH;
     protected int RESHEIGHT = VisionConstants.RESHEIGHT;
     protected int TIMEOUT = 5;
-    protected CircleDetectionPipeline circleDetection;
+//    protected CircleDetectionPipeline circleDetection;
 //    protected SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     protected Pose2d startPose;
     protected boolean isRed;
@@ -40,7 +40,7 @@ public class AutonomousOpmode extends LinearOpMode {
     protected TrajectorySequence center;
     protected TrajectorySequence right;
 
-    protected AutonomousTrajectories trajectories;
+//    protected AutonomousTrajectories trajectories;
     protected VisionFunctions visionFunctions;
     protected CircleDetectionPipeline circleDetectionPipeline;
     protected DeliveryFunctions deliveryFunctions;
@@ -51,17 +51,16 @@ public class AutonomousOpmode extends LinearOpMode {
 
 
     protected void Initialize(LinearOpMode l) {
-        circleDetectionPipeline = new CircleDetectionPipeline(l.telemetry, isRed);
+//        circleDetectionPipeline = new CircleDetectionPipeline(l.telemetry, isRed);
 
-        visionFunctions = new VisionFunctions(l);
         intakeFunctions = new IntakeFunctions(l);
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        webcam.setPipeline(circleDetectionPipeline);
-
+//        webcam.setPipeline(circleDetectionPipeline);
 //        drive = new SampleMecanumDrive(hardwareMap);
+
         deliveryFunctions = new DeliveryFunctions(l, true);
         visionFunctions = new VisionFunctions(l);
 
@@ -103,48 +102,6 @@ public class AutonomousOpmode extends LinearOpMode {
             drivetrainFunctions.Move(moveProfile[0], moveProfile[1], moveProfile[2], 1);
         }
     }
-
-    //    if (aprilTagsFunctions.DetectAprilTag(aprilTagsFunctions.BLUE_1_TAG)) {
-//        telemetry.addData("Found", "ID %d (%s)", aprilTagsFunctions.detectedTag.id, aprilTagsFunctions.detectedTag.metadata.name);
-//        telemetry.addData("Range", "%5.1f inches", aprilTagsFunctions.detectedTag.ftcPose.range);
-//        telemetry.addData("Bearing", "%3.0f degrees", aprilTagsFunctions.detectedTag.ftcPose.bearing);
-//        telemetry.addData("Yaw", "%3.0f degrees", aprilTagsFunctions.detectedTag.ftcPose.yaw);
-//        telemetry.addData("X delta", "%3.0f inches", aprilTagsFunctions.detectedTag.ftcPose.x);
-//
-//        if (gamepad1.right_trigger > 0.025f) {
-//            rightTriggerPull = gamepad1.right_trigger;
-//
-////                    strafeGain *= rightTriggerPull;
-////                    forwardGain *= rightTriggerPull;
-////                    rotationGain *= rightTriggerPull;
-//
-//            double x = STRAFE_GAIN * aprilTagsFunctions.detectedTag.ftcPose.yaw;
-//            double y = -FORWARD_GAIN * aprilTagsFunctions.detectedTag.ftcPose.range;
-//
-////                    double x = 0.5;
-////                    double y = 0.7;
-//            double bearing = -ROTATION_GAIN * aprilTagsFunctions.detectedTag.ftcPose.bearing;
-//
-//            telemetry.addData("x: ", x);
-//            telemetry.addData("y: ", y);
-//            telemetry.addData("bearing: ", bearing);
-//
-//            drivetrainFunctions.Move((float) x, (float) y, (float) bearing, 1);
-//        } else {
-//            controlsRelinquished = false;
-//        }
-//
-//                /*
-//                if (currentGamepad1.right_trigger > 0.5) {
-//                    y      = SPEED_GAIN * (aprilTagsFunctions.detectedTag.ftcPose.range - DESIRED_DISTANCE_TO_APRIL_TAG_INCHES);
-//                    yaw    = -TURN_GAIN * aprilTagsFunctions.detectedTag.ftcPose.yaw;
-//                    x      = STRAFE_GAIN * aprilTagsFunctions.detectedTag.ftcPose.x;
-//                    isAutoDrivingToAprilTag = true;
-//                }
-//                 */
-//    } else {          //align to point (pose of aprilTag)
-//
-//    }
     public void runOpMode() {
         waitForStart();
     }
