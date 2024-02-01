f9613172b5cf8cf2de768c65801eb54d2708adaf
HuyStenzel <Stenz00697@loswego.k12.or.us>
2024-02-01 10:18:24 -0800

    Added touchpad controlls

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchpadFunctions.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+@Disabled
+public class TouchpadFunctions {
+    private Gamepad prevGamepad;
+    private Gamepad currentGamepad;
+
+    private LinearOpMode linearOpMode;
+    private Gamepad gamepad;
+    private int gamepadNum;
+    private float onPressX;
+    private float onPressY;
+
+    private float XSwipe;
+    private float YSwipe;
+
+
+    public TouchpadFunctions(LinearOpMode l, int gamepadNumber)
+    {
+        linearOpMode = l;
+        Initialize();
+        gamepadNum = gamepadNumber;
+
+    }
+
+
+    private void Initialize(){
+        if(gamepadNum == 1){
+            gamepad = linearOpMode.gamepad1;
+        } else {
+            gamepad = linearOpMode.gamepad2;
+        }
+    }
+
+    private void Reinitialize(){
+        if(gamepadNum == 1){
+            gamepad = linearOpMode.gamepad1;
+        } else {
+            gamepad = linearOpMode.gamepad2;
+        }
+    }
+
+    public boolean getTouchpadDown(){
+         gamepad.copy(currentGamepad);
+
+         if(prevGamepad != null && !prevGamepad.touchpad_finger_1 && currentGamepad.touchpad_finger_1){
+             //DOWN
+             onPressX = currentGamepad.touchpad_finger_1_x;
+             onPressY = currentGamepad.touchpad_finger_1_y;
+             return true;
+         } else{
+             return false;
+         }
+    }
+
+    public boolean getTouchpadUp(){
+        gamepad.copy(currentGamepad);
+        if(prevGamepad != null && prevGamepad.touchpad_finger_1 && !currentGamepad.touchpad_finger_1){
+            //UP
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public void updateTouchpadStateAtEndOfLoop(){
+        gamepad.copy(prevGamepad);
+    }
+
+    public float getXSwipe(){
+        XSwipe = currentGamepad.touchpad_finger_1_x - onPressX;
+        return XSwipe;
+    }
+
+    public float getYSwipe(){
+        YSwipe = currentGamepad.touchpad_finger_1_y - onPressY;
+        return YSwipe;
+    }
+
+}
