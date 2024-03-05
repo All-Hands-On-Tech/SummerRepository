e34fed1d9e48113725ee181b8a0311628eb9e742
HuyStenzel <Stenz00697@loswego.k12.or.us>
2024-03-05 08:26:53 -0800

    servoDodge change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeliveryFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeliveryFunctions.java
@@ -32,7 +32,7 @@ public class DeliveryFunctions {
     public final double servoIn = 0.719;
     private final double servoOut = 0.767;//0.77
 
-    public final double servoDodge = 0.704;//0.421
+    public final double servoDodge = 0.71;//0.421
 
     private int targetPosition;
     private double currentPosition;
