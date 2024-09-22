7c63a0852fb165774b2373aba91fa71206d65892
BHill256 <bricelikesrobots@gmail.com>
2024-09-22 14:32:05 -0700

    Created red auto paths in MM

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,12 +16,39 @@ public class MeepMeepTesting {
                 // We set this bot to be red
                 //bot is 18x18 box
                 .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(10, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(24+9, -70+9, Math.toRadians(90)))
-                .splineTo(new Vector2d(10, 10), 0)
+//        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(25, -62, Math.toRadians(90)))
+//                .strafeTo(new Vector2d(10, -34))
+//                //score specimen
+//                .strafeTo(new Vector2d(20, -45))
+//                .setTangent(-45)
+//                .splineToLinearHeading(new Pose2d(45, -14, Math.toRadians(90)), Math.toRadians(-70))
+//                .strafeTo(new Vector2d(45, -59))
+//                .setTangent(90)
+//                .splineToLinearHeading(new Pose2d(55.00, -11, Math.toRadians(90)), Math.toRadians(-90))
+//                .strafeTo(new Vector2d(55, -60))
+//                .setTangent(90)
+//                .splineToSplineHeading(new Pose2d(62, -24.10, Math.toRadians(180)), Math.toRadians(0))
+//                .strafeTo(new Vector2d(62, -62))
+//                .build());
+
+        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(-14, -62, Math.toRadians(90)))
+                .strafeTo(new Vector2d(-10, -34))
+//               //score specimen
+                .strafeTo(new Vector2d(-26, -40))
+                .setTangent(110)
+                .splineToLinearHeading(new Pose2d(-45, -14, Math.toRadians(90)), Math.toRadians(-120))
+                .splineToLinearHeading(new Pose2d(-58, -58, Math.toRadians(45)), Math.toRadians(225))
+                .strafeTo(new Vector2d( -40, -27))
+                .splineToLinearHeading(new Pose2d(-57.00, -14, Math.toRadians(90)), Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(-58, -58, Math.toRadians(60)), Math.toRadians(240))
+                .strafeTo(new Vector2d( -50, -27))
+                .splineToLinearHeading(new Pose2d(-62.00, -24, Math.toRadians(0)), Math.toRadians(180))
+                .strafeTo(new Vector2d(-62, -55))
                 .build());
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
