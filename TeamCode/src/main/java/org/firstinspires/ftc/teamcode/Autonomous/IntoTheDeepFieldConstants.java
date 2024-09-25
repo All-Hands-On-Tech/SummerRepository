fe13b240a17a2fa01b3a8dd00aeb16ed5e5afb32
stenzelkhai <stenzelkhai@gmail.com>
2024-09-25 11:51:34 -0700

    Update IntoTheDeepFieldConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IntoTheDeepFieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IntoTheDeepFieldConstants.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.acmerobotics.roadrunner.Vector2d;
+
 import java.util.Vector;
 
 public class IntoTheDeepFieldConstants {
-//    public static final SPIKE_1_POS = new Vector<double>(0.0, 0.0);
+    public static final Vector2d SPIKE_1_POS = new Vector2d(0.0, 0.0);
+    public static final Vector2d SPIKE_2_POS = new Vector2d(0.0, 0.0);
+    public static final Vector2d SPIKE_3_POS = new Vector2d(0.0, 0.0);
 }
