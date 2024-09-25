82b1a1e838f06159cdfd80230f1a79c9a212ce35
stenzelkhai <stenzelkhai@gmail.com>
2024-09-24 19:53:43 -0700

    Gradle angry at me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IntoTheDeepFieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IntoTheDeepFieldConstants.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import java.util.Vector;
 
 public class IntoTheDeepFieldConstants {
-    public static final SPIKE_1_POS = new Vector<double>(0.0, 0.0);
+//    public static final SPIKE_1_POS = new Vector<double>(0.0, 0.0);
 }
