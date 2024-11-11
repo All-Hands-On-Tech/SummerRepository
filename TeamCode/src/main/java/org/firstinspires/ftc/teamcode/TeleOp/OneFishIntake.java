ad597900d1337193d7a6f72a770708cabd20f3fb
HuyStenzel <Stenz00697@loswego.k12.or.us>
2024-11-10 22:35:44 -0800

    Added one Fish code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/OneFishIntake.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Disabled
+public class OneFishIntake {
+    private DcMotor extension = null;
+    private DcMotor intake = null;
+    private Servo pitch = null;
+
+    private LinearOpMode linearOpMode;
+
+    public final double CLICKS_PER_DEGREE = 3.5;
+    private final double CLICKS_PER_CM = 24.92788;
+    private final int MM_PER_METER = 1000;
+
+    private final int MIN_EXTENSION = 0;
+    private final int MAX_EXTENSION = 1000;
+    private final double MIN_PITCH = 0;
+    private final double MAX_PITCH = 1;
+
+    private final int TICK_LOW_POWER_DISTANCE = 200;
+
+    private double targetLengthCM;
+    private int targetLength;
+    private double currentPosition;
+
+    public final double TICK_STOP_THRESHOLD = 20;
+
+    private ElapsedTime time = new ElapsedTime();
+
+
+
+    public OneFishIntake(LinearOpMode l)
+    {
+        linearOpMode = l;
+        Initialize();
+    }
+
+
+    private void Initialize(){//4mm
+        try {
+            extension = linearOpMode.hardwareMap.get(DcMotor.class, "extension");
+            extension.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            extension.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            extension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            intake = linearOpMode.hardwareMap.get(DcMotor.class, "intake");
+            intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            pitch = linearOpMode.hardwareMap.get(Servo.class, "pitch");
+            pitch.scaleRange(0.0, 1.0);
+
+//            pitchMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+
+        }catch(NullPointerException e){
+            linearOpMode.telemetry.addLine("Couldn't find intake");
+        }
+    }
+
+
+    public void setTargetLength(int ticks){
+        targetLength = ticks;
+        extension.setTargetPosition(targetLength);
+    }
+    public void updateLength(){
+        linearOpMode.telemetry.addData("Intake Length: ", targetLength);
+        if(Math.abs(extension.getCurrentPosition() - extension.getTargetPosition()) > TICK_STOP_THRESHOLD){
+            extension.setPower(1);
+        } else{
+            extension.setPower(0);
+        }
+    }
+
+    public void setIntakePower(float power){
+        intake.setPower(power);
+    }
+
+    public void setPitch(float position){
+        pitch.setPosition(position);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/OneFishTeleop.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.DrivetrainFunctions;
+import org.firstinspires.ftc.teamcode.Intake;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@TeleOp(name="OneFish Teleop", group="AAA")
+public class OneFishTeleop extends LinearOpMode {
+
+    private ElapsedTime deliveryTimer = new ElapsedTime();
+
+    DrivetrainFunctions drivetrainFunctions = null;
+
+    OneFishIntake intake = null;
+
+    private final double CLICKS_PER_CM = 24.92788;
+
+    private final double HOME_X = 50;
+    private final double HOME_Y = -20;
+
+    private final double STORE_X = -35.94;
+    private final double STORE_Y = 20.75;
+
+    private double xOffset = 0.0;
+    private double yOffset = 0.0;
+
+    private Pose2d initPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+    private Pose2d prevPoseEstimate = new Pose2d(0,0,0);
+    private Pose2d poseEstimate = new Pose2d(0,0,0);
+
+    private boolean isGlobalTargeting = false;
+    private double prevEncoderX = 0.0;
+    private double encoderX = 0.0;
+    private double deltaX = 0.0;
+
+    private int targetExtention = 0;
+
+    private boolean controlsRelinquished = false;
+    private final double DRIVE_DEADZONE = 0.05;
+    private final double SCORE_SPEED_SCALAR = 0.2;
+
+    private double speedScalar = 1;
+
+    //This is the code to add rr actions
+    private FtcDashboard dash = FtcDashboard.getInstance();
+    private List<Action> runningActions = new ArrayList<>();
+    @Override
+    public void runOpMode() {
+
+        drivetrainFunctions = new DrivetrainFunctions(this);
+        intake = new OneFishIntake(this);
+
+//        MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+//            drive.updatePoseEstimate();
+//            poseEstimate = drive.pose;
+//            encoderX = drivetrainFunctions.getX();
+
+            TelemetryPacket packet = new TelemetryPacket();
+
+            //driver 1
+            //slow down power if bumper is pressed
+            if (gamepad1.left_bumper) {
+                speedScalar = 0.5;
+            } else if (gamepad1.right_bumper) {
+                speedScalar = 0.8;
+            } else {
+                speedScalar = 1;
+            }
+
+            if (!controlsRelinquished) {
+                float leftX = gamepad1.left_stick_x;
+                float leftY = gamepad1.left_stick_y;
+                float rightX = gamepad1.right_stick_x;
+                float rightY = gamepad1.right_stick_y;
+                if (Math.abs(leftX) > DRIVE_DEADZONE || Math.abs(leftY) > DRIVE_DEADZONE || Math.abs(rightX) > DRIVE_DEADZONE || Math.abs(rightY) > DRIVE_DEADZONE*2) {
+                    if(Math.abs(rightY) > DRIVE_DEADZONE*2) {
+                        drivetrainFunctions.Move(leftX, rightY, rightX, speedScalar);
+                    }else{
+                        drivetrainFunctions.Move(leftX, leftY, rightX, speedScalar);
+                    }
+                } else {
+                    drivetrainFunctions.Stop();
+                }
+            }
+
+            //driver 2
+            if(!controlsRelinquished) {
+                float leftX = gamepad2.left_stick_x;
+                float leftY = -gamepad2.left_stick_y;
+                float rightX = gamepad2.right_stick_x;
+                float rightY = -gamepad2.right_stick_y;
+
+                if(Math.abs(leftY) > DRIVE_DEADZONE){
+                    targetExtention += (int)leftY;
+                }
+
+                if(gamepad2.x){
+                    intake.setIntakePower(1);
+                }
+                if(gamepad2.b){
+                    intake.setIntakePower(0);
+                }
+                intake.setTargetLength(targetExtention);
+                intake.updateLength();
+
+
+            }
+
+            telemetry.update();
+
+            //Adds rr actions
+            List<Action> newActions = new ArrayList<>();
+            for (Action action : runningActions) {
+                action.preview(packet.fieldOverlay());
+                if (action.run(packet)) {
+                    newActions.add(action);
+                }
+            }
+            runningActions = newActions;
+
+            dash.sendTelemetryPacket(packet);
+
+//            prevPoseEstimate = poseEstimate;
+            prevEncoderX = encoderX;
+        }
+    }
+}
