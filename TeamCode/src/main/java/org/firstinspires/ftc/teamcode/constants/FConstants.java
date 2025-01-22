f8033c08f3cd1bb03fc6ee313f949e7a1342950a
All Hands On Tech <TeamAllHandsOnTech@gmail.com>
2025-01-21 16:27:57 -0800

    got rid the compilation errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/FConstants.java
@@ -1,4 +1,4 @@
-package pedroPathing.constants;
+package org.firstinspires.ftc.teamcode.constants;
 
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
@@ -10,17 +10,17 @@ public class FConstants {
     static {
         FollowerConstants.localizers = Localizers.THREE_WHEEL;
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+        FollowerConstants.leftFrontMotorName = "LF";
+        FollowerConstants.leftRearMotorName = "LBLE";
+        FollowerConstants.rightFrontMotorName = "RFBE";
+        FollowerConstants.rightRearMotorName = "RBRE";
 
         FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 10.5 * 2.20462;
 
         FollowerConstants.xMovement = 57.8741;
         FollowerConstants.yMovement = 52.295;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/LConstants.java
@@ -1,22 +1,22 @@
-package pedroPathing.constants;
+package org.firstinspires.ftc.teamcode.constants;
 
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelConstants.forwardTicksToInches = .001989436789; /** use individual tuning programs*/
         ThreeWheelConstants.strafeTicksToInches = .001989436789;
         ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        ThreeWheelConstants.leftY = 7.75;  /** need to be physically measured*/
+        ThreeWheelConstants.rightY = -7.75;
+        ThreeWheelConstants.strafeX = -5.5;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "LBLE";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "RFBE";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "RBRE";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE; /** use Localization Test*/
+        ThreeWheelConstants.rightEncoderDirection = Encoder.FORWARD;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
     }
 }
 
